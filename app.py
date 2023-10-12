e6e33f2d0a7d139911e4cb7a3dfbd3021b69c4d7
Abdo-Mos <aabbddoo.sshh@gmail.com>
2023-10-11 20:05:44 -0400

    feat: CRUD create

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import requests
 
 app = Flask(__name__)
 
-# USR_URL_REQ =
+URL_usr_req  = 'http://127.0.0.1:5000/user/'
+URL_post_req = 'http://127.0.0.1:5001/post/'
 
 comments = [
     {"id": 1, "user_id": 1, "post_id": 2, "comment": "Amazing post!"},
-    {"id": 2, "user_id": 2, "post_id": 1, "comment": "Another amazing post!"},
+    {"id": 2, "user_id": 2, "post_id": 2, "comment": "Another amazing post!"},
 ]
 
 @app.route('/')
@@ -24,15 +25,30 @@ def get_by_id(id):
             comment_info = comment
             break
     
-    usr_req_res = requests.get(f'http://127.0.0.1:5000/user/{comment_info["user_id"]}').json()
-    post_req_res = requests.get(f'http://127.0.0.1:5001/post/{comment_info["post_id"]}').json()
+    usr_req_res = requests.get(f'{URL_usr_req}{comment_info["user_id"]}').json()
+    post_req_res = requests.get(f'{URL_post_req}{comment_info["post_id"]}').json()
 
     comment_info['user'] = usr_req_res
     comment_info['post'] = post_req_res
     
     return jsonify(comment_info)
 
+# -C- craete comments
+@app.route('/comment', methods=['POST'])
+def craete_comment():
 
+    usr_req_res = requests.get(f'{URL_usr_req}{request.json["user_id"]}').json()
+    post_req_res = requests.get(f'{URL_post_req}{request.json["post_id"]}').json()
+
+    new_comment = {
+    "id": request.json['id'],
+    "user_id": usr_req_res,
+    "post_id": post_req_res,
+    "comment": request.json['comment'] 
+    }
+
+    comments.append(new_comment)
+    return jsonify({'Success': new_comment})
 
 
 
